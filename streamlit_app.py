4207f155c9a14cb338b9ecfb2f82028961fbd726
Mohammad Taleb <240728+talebbits@users.noreply.github.com>
2023-11-22 11:05:11 -0500

    add 2nd input

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,4 @@ my_data_rows = my_cur.fetchall()
 
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+add_my_fruit = my_fruit_list.set_index('What Fruit would you like to add')
